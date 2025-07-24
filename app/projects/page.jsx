3268bf2070282b7ce43aacb9a5cdecb9e1816d8a
jsonwang2003 <jason0100w@gmail.com>
2025-07-23 19:54:42 -0700

    Projects initial Commit

---
+++ app/projects/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography, Container, Button, Box } from '@mui/material'
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <Container maxWidth="md">
+    </Container>
+  )
+}
