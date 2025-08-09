b9beb1227f3464aba677b6deac3b5041bf5ff43a
jsonwang2003 <jason0100w@gmail.com>
2025-08-08 17:16:40 -0700

    Initial Calendar Commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,20 @@
-import { Container } from '@mui/material'
+import { Container, Box } from '@mui/material'
 
 function Home() {
   return(
     <Container>
       <h1>Welcome to OC Robotics</h1>
       <p>Your one-stop solution for all things robotics.</p>
+      <Box sx={{ width: '100%', height: 600 }}>
+        <object
+          type="text/html"
+          data="https://calendar.google.com/calendar/embed?src=ocroboticsgroup%40gmail.com&ctz=America%2FLos_Angeles&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0"
+          title="OC Robotics Calendar"
+          width="100%"
+          height="100%"
+          style={{ border: 'none' }}
+        />
+      </Box>
     </Container>
   )
 }
